17622eb41645a8776f1e030dd2c81af34f3d2942
mfon_ <mfonezekel@gmail.com>
2025-05-31 22:07:15 -0700

    Initial Commit

---
+++ Tune/TuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuneApp.swift
+//  Tune
+//
+//  Created by Mfon Udoh on 2025-05-31.
+//
+
+import SwiftUI
+
+@main
+struct TuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TuneTests/TuneTests.swift
@@ -0,0 +1,17 @@
+//
+//  TuneTests.swift
+//  TuneTests
+//
+//  Created by Mfon Udoh on 2025-05-31.
+//
+
+import Testing
+@testable import Tune
+
+struct TuneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
